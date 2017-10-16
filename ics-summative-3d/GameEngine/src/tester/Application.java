cb832e2362f8062b4f5c21a57036b894e5f3af2d
Tianhao Wang <tianhao2k15@gmail.com>
2017-10-16 11:09:24 -0400

    Test

--- ics-summative-3d/GameEngine/src/tester/Application.java
+++ ics-summative-3d/GameEngine/src/tester/Application.java
@@ -13,6 +13,9 @@ import textures.Texture;
 public class Application {
 
 	public static void main(String[] args) {
+
+		//this is a test
+
 		Displayer.create();
 		
 		Loader loader = new Loader();
