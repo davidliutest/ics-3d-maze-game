6efee596f1f18ad3354f41ad6687b21bb9045cb3
davidliutest <davidliu7h@gmail.com>
2017-10-16 11:39:18 -0400

    Redo

---
+++ Game/src/render/Renderer.java
@@ -0,0 +1,31 @@
+package render;
+
+import models.Model;
+import models.TextureModel;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL13;
+import org.lwjgl.opengl.GL20;
+import org.lwjgl.opengl.GL30;
+
+public class Renderer {
+	
+	public void start() {
+		// 1 is maximum for these RGB vals
+		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
+		GL11.glClearColor(1, 0, 0, 1);
+	}
+	
+	public void render(TextureModel textureModel) {
+		Model model =  textureModel.getModel();
+		GL30.glBindVertexArray(model.getVaoID());
+		GL20.glEnableVertexAttribArray(0);
+		GL20.glEnableVertexAttribArray(1);
+		GL13.glActiveTexture(GL13.GL_TEXTURE0);
+		GL11.glBindTexture(GL11.GL_TEXTURE_2D, textureModel.getTexture().getID());
+		GL11.glDrawElements(GL11.GL_TRIANGLES, model.getVertexCount(), GL11.GL_UNSIGNED_INT, 0);
+		GL20.glDisableVertexAttribArray(0);
+		GL20.glDisableVertexAttribArray(1);
+		GL30.glBindVertexArray(0);
+	}
+
+}
